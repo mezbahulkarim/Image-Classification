f21bb16c5e1de57f4a12735981f735ecf0612ec2
mezbahul <mezbahul@kth.se>
2022-03-23 21:52:37 +0100

    readme update

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ import numpy as np
 data = datasets.cifar10.load_data()
 (x_train, y_train), (x_test, y_test) = data
 
-#x_test.shape    #images, dimension, dimension, rgb channels
+#x_test.shape                        #images, dimension, dimension, rgb channels
 #y_train[:5]
-y_train=y_train.reshape(-1, )       #convert 2d array to 1d
+y_train=y_train.reshape(-1, )        #convert 2d array to 1d
 y_train[:5]
 
-#plt.figure(figsize=(15,2))
-#plt.imshow(x_train[50])
+#plt.figure(figsize=(15,2))     
+#plt.imshow(x_train[50])             #change index see image
 
 # %%
    
