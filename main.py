a6cc68583c155e0ac01c6d1f38c49c5acf8e0014
mezbahul <mezbahul@kth.se>
2022-03-23 21:44:13 +0100

    small update

---
+++ main.py
@@ -0,0 +1,28 @@
+# %%
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+# %%
+data = datasets.cifar10.load_data()
+(x_train, y_train), (x_test, y_test) = data
+
+#x_test.shape    #images, dimension, dimension, rgb channels
+#y_train[:5]
+y_train=y_train.reshape(-1, )       #convert 2d array to 1d
+y_train[:5]
+
+#plt.figure(figsize=(15,2))
+#plt.imshow(x_train[50])
+
+# %%
+   
+
+
+
+
+
+
